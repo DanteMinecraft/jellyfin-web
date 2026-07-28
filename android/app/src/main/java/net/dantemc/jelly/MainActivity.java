6b52e8659a17952c3ad8057f88df4ce4543dcf81
DanteMinecraft <dantewallen07@gmail.com>
2026-07-28 17:37:56 +0200

    Add Capacitor Android support

---
+++ android/app/src/main/java/net/dantemc/jelly/MainActivity.java
@@ -0,0 +1,5 @@
+package net.dantemc.jelly;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'net.dantemc.jelly',
+  appName: 'Jellyfin: Civitas Edition',
+  webDir: 'dist'
+};
+
+export default config;
